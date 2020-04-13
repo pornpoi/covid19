1721bda6ddc61efbb5b0669da1f71dea9d2f93b7
pornpong <pornpong.i@pttdigital.com>
2020-04-13 11:22:41 +0700

    Updates

---
+++ precache-manifest.e8a438bd075364e834732e6e4c24ef88.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adda6f833dc902467081cf25332c3a7",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "f0457ff58fc1d59c80cf",
+    "url": "/covid19/static/css/2.367471c6.chunk.css"
+  },
+  {
+    "revision": "583f062bd41202ef616e",
+    "url": "/covid19/static/css/main.f1e1bee9.chunk.css"
+  },
+  {
+    "revision": "f0457ff58fc1d59c80cf",
+    "url": "/covid19/static/js/2.8786fc70.chunk.js"
+  },
+  {
+    "revision": "583f062bd41202ef616e",
+    "url": "/covid19/static/js/main.b4f24727.chunk.js"
+  },
+  {
+    "revision": "9edb58dfee5d88d19d35",
+    "url": "/covid19/static/js/runtime~main.60db82c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/covid19/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/covid19/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/covid19/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/covid19/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
